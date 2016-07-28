c1bc0c5e2db3b0fae50fa0794f6002b6ae9ce00b
rscsam <sam.costley@yahoo.com>
2016-07-28 19:58:33 -0400

    First complete commit.

---
+++ app/src/main/java/com/atbash_crypt/sylan/atbashcrypt/Atbash.java
@@ -0,0 +1,35 @@
+package com.atbash_crypt.sylan.atbashcrypt;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Atbash {
+    private static ArrayList<Character> ALPHABET;
+
+    static {
+        ArrayList<Character> temp = new ArrayList<Character>();
+        for (int i = 0; i < 26; i++) {
+        	temp.add((char) ('A' + i));
+        }
+        ALPHABET = temp;
+    }
+    
+    public static String encrypt(String text) {
+    	text = text.toUpperCase();
+        String result = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (ALPHABET.contains(text.charAt(i))) {
+                    result += ALPHABET.get((25 - ALPHABET.indexOf(text.charAt(i))) 
+                    		% 26);
+            } else {
+                result += text.charAt(i);
+            }
+        }
+        return result;
+    }
+    
+    public static String decrypt(String text) {
+    	return Atbash.encrypt(text);
+    }
+}
